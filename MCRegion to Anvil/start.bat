9757025d64568b46a2337467d17192c4f9efaf81
tungstenwire <85665713+tungstenwire@users.noreply.github.com>
2021-08-10 19:31:55 -0700

    Update start.bat

--- MCRegion to Anvil/start.bat
+++ MCRegion to Anvil/start.bat
@@ -1,3 +1,3 @@
-SET worldpath="C:\Users\wacj\Desktop\Minecraft Maps\world"
+SET worldpath="C:\Users\wacj\Desktop\Minecraft Maps\MCRegion to Anvil"
 java -jar AnvilConverter.jar %worldpath% world
 PAUSE
